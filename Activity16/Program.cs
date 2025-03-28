4afa12e04a726ad6ae64a9e2add55f344a545bf5
pc <pc@DESKTOP-PGJ17C9>
2025-03-28 16:41:44 +1300

    Add project files.

---
+++ Activity16/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            WriteFriendsToFile();
+        }
+
+        static void WriteFriendsToFile()
+        {
+            string[] friends = { "\n Alice", "\n Bob", "\n Charlie", "\n David", "\n Eve" };
+            File.WriteAllLines("friends.txt", friends);
+            Console.WriteLine("Friends' names written to file.");
+        }
+    }
+}
